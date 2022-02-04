a9ca43f12cbef48081dcc6f929d432f5ac2ab991
Jamon Holmgren <jamonholmgren@gmail.com>
2022-02-04 09:14:12 -0800

    Initial Commit

---
+++ SecretSanta/SecretSantaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretSantaApp.swift
+//  SecretSanta
+//
+//  Created by Jamon Holmgren on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct SecretSantaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
